fc09e04c643de77be0e4a2a1659f07e5f18d8be6
Shayan Arman <shayan.arman2@gmail.com>
2026-02-22 14:36:59 -0800

    remove: unused constants

--- src/constants/Constants.ts
+++ src/constants/Constants.ts
@@ -5,20 +5,3 @@ export const COMPANY_NAME = "CONFORMITAS LTD.";
 export const APP_NAME = "Exa Compliance";
 
 export const PARTNER_REF_KEY = "campaign-ref";
-
-export const LOGIN_ERROR_MISSING_SCOPES = "incorrect-scopes";
-
-export const LOGIN_ERROR_DELETE_IN_PROGRESS = "delete-progress";
-
-export const LOGIN_ERROR_EXPIRED_GMAIL_TOKEN = "expired-gmail-login";
-
-export const LOGIN_ERROR_HOTMAIL_IN_GMAIL = "hotmail-in-gmail";
-
-export const LOGIN_ERROR_UNKNOWN = "unknown-login-error";
-
-export const NO_MARKETING_PROGRAM_SLUG = 'no_marketing_program';
-
-export const NOT_ENOUGH_CREDITS = "not_enough_credits";
-
-export const DOUBLE_CHECK_PERSON_THRESHOLD = 0.5;
-export const REAL_PERSON_THRESHOLD = 0.8;
